534c43aed7c2b8bde3b5ce91f16fe6eaa9ee0de1
2002OID <ykkim308321@gmail.com>
2023-09-21 10:03:58 +0900

    test_draw

--- character_rec_cir_move.py
+++ character_rec_cir_move.py
@@ -11,10 +11,18 @@ def rec_move():
 
 def cir_move():
     print('cir')
+    #일단 그림부터 그려보자
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
+
+    
     pass
 
 while True:
     rec_move()
     cir_move()
+    break
     
 close_canvas()

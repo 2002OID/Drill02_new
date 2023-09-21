0911fc8b37f758f2cf3e30569b87d0bca25dacd4
2002OID <ykkim308321@gmail.com>
2023-09-21 10:13:13 +0900

    make_cir_move

--- character_rec_cir_move.py
+++ character_rec_cir_move.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -12,13 +13,17 @@ def rec_move():
 def cir_move():
     print('cir')
     #일단 그림부터 그려보자
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(400,90)
-    delay(1)
-
     
-    pass
+    #x = r * cos(s), y = r * sin(s)
+    cx, cy, r = 400, 300 ,210
+    for deg in range(0, 360, 5):
+        x = r * math.cos(math.radians(deg))
+        y = r * math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x + cx,y + cy)
+        delay(0.01)
+        pass
 
 while True:
     rec_move()

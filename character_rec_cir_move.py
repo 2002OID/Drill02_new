764ff16f00a138e17add21d21d9fc2f619ed4881
2002OID <ykkim308321@gmail.com>
2023-09-21 09:55:12 +0900

    def funtion

--- character_rec_cir_move.py
+++ character_rec_cir_move.py
@@ -5,6 +5,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def rec_move():
+    pass
+
+def cir_move():
+    pass
+
 while True:
     rec_move()
     cir_move()
